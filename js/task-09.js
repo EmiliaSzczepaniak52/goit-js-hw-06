46189231b17219358ec955b0ec3797aadf0f9798
EmiliaSzczepaniak52 <emiliaszczepaniak52@gmail.com>
2023-03-06 23:12:48 +0100

    Update all

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-const bgColor = document.querySelector("body");
-const btnChangeColor = document.querySelector(".change-color");
-const nameBgColor = document.querySelector('.color');
-
-
-btnChangeColor.addEventListener("click", (event)=>{   
-    const randomColor = getRandomHexColor();
-    bgColor.style.backgroundColor = randomColor;
-    nameBgColor.textContent = randomColor;
-});
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  
+  const bgColor = document.querySelector("body");
+  const btnChangeColor = document.querySelector(".change-color");
+  const nameBgColor = document.querySelector('.color');
+  
+  
+  btnChangeColor.addEventListener("click", (event)=>{   
+      const randomColor = getRandomHexColor();
+      bgColor.style.backgroundColor = randomColor;
+      nameBgColor.textContent = randomColor;
+  });
+  
